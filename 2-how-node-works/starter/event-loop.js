7d3de8667fe6d3632cbea6bde263bacd4f7226fa
anurag47103 <anuragsharma47103@gmail.com>
2022-11-18 04:40:57 +0530

    sync method of encryption

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const start = Date.now();
-process.env.UV_THREADPOOL_SIZE = 2;
+process.env.UV_THREADPOOL_SIZE = 4;
 
 //2nd the timeout calback will run
 setTimeout(() => console.log(`Timer 1 finished`), 1000);
@@ -18,21 +18,14 @@ fs.readFile('text-file.txt', () => {
 
 	process.nextTick(() => console.log('Process.nextTick'));
 
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-		console.log(Date.now() - start, 'Password encrypted');
-	});
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-		console.log(Date.now() - start, 'Password encrypted');
-	});
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-		console.log(Date.now() - start, 'Password encrypted');
-	});
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-		console.log(Date.now() - start, 'Password encrypted');
-	});
-	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
-		console.log(Date.now() - start, 'Password encrypted');
-	});
+	crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+	console.log(Date.now() - start, 'Password encrypted');
+	crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+	console.log(Date.now() - start, 'Password encrypted');
+	crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+	console.log(Date.now() - start, 'Password encrypted');
+	crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+	console.log(Date.now() - start, 'Password encrypted');
 });
 
 //1st the top level code will run
