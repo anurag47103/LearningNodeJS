ff2a8b552b49adab245fe5e40765b093feb6cf78
anurag47103 <anuragsharma47103@gmail.com>
2022-11-18 04:38:30 +0530

    setting the number of Threads in thread pool

--- 2-how-node-works/starter/event-loop.js
+++ 2-how-node-works/starter/event-loop.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 2;
 
 //2nd the timeout calback will run
 setTimeout(() => console.log(`Timer 1 finished`), 1000);
@@ -13,6 +17,22 @@ fs.readFile('text-file.txt', () => {
 	setImmediate(() => console.log('Immediate 2 finished'));
 
 	process.nextTick(() => console.log('Process.nextTick'));
+
+	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+		console.log(Date.now() - start, 'Password encrypted');
+	});
+	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+		console.log(Date.now() - start, 'Password encrypted');
+	});
+	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+		console.log(Date.now() - start, 'Password encrypted');
+	});
+	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+		console.log(Date.now() - start, 'Password encrypted');
+	});
+	crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+		console.log(Date.now() - start, 'Password encrypted');
+	});
 });
 
 //1st the top level code will run
